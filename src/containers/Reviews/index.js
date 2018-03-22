b79a5994bc9cd36bec1981de8819f368deea5095
hazratgs <hazratgs@gmail.com>
2018-03-22 13:53:09 +0300

    add default state

--- src/containers/Reviews/index.js
+++ src/containers/Reviews/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import CSSModules from 'react-css-modules'
 import s from './style.pcss'
@@ -10,9 +11,31 @@ import GoalIcon from '../../public/svg/goal.svg'
 import RIcon from '../../public/svg/r.svg'
 
 @withRouter
+@connect(
+  state => ({
+    reviews: state.App.reviews,
+    activeReview: state.App.activeReview
+  })
+)
 @CSSModules(s)
 export default class Reviews extends PureComponent {
   render () {
+    const [active] = this.props.reviews.filter(item => item.id === this.props.activeReview)
+    const reviews = this.props.reviews.map(item =>
+      <button
+        key={item.id}
+        styleName='item' className={`${s[item.color]} ${active.id === item.id && s.active}`}
+      >
+        <div styleName='face'>
+          <img src={item.face} />
+        </div>
+        <div styleName='name'>
+          <span>{item.type}</span>
+          <p>{item.name}</p>
+        </div>
+      </button>
+    )
+
     return (
       <div styleName='reviews'>
         <div styleName='head'>
@@ -29,13 +52,15 @@ export default class Reviews extends PureComponent {
         <div styleName='content'>
           <div styleName='wrapper'>
             <div styleName='img'>
-              <img src='https://ae01.alicdn.com/kf/HTB1KlsZNVXXXXa5apXXq6xXFXXX1/-.jpg_640x640.jpg' />
+              <img src={active.img} />
               <div styleName='bottom'>
-                <button styleName='video'>
-                  <VideoIcon/>
-                </button>
+                {active.video && (
+                  <button styleName='video'>
+                    <VideoIcon />
+                  </button>
+                )}
                 <p styleName='user'>
-                  Константина Константинопольская
+                  {active.name}
                 </p>
               </div>
             </div>
@@ -45,52 +70,17 @@ export default class Reviews extends PureComponent {
                   <GoalIcon/>
                   <span>Цель</span>
                 </div>
-                <p>Пробеги полумарафон в Вероне</p>
+                <p>{active.title}</p>
               </div>
               <div styleName='text'>
-                <p>Очень долго хотела скинуть лишний вес да и вообще начать вести здоровый образ жизни. Но все было некогда. Знакомые позвали на открытую тренировку в школу бега с мастером спорта. Чем черт не шутит, решила сходить, тем более интересно было узнать, почему.</p>
+                <p>{active.text}</p>
                 <Link to='/'>Читать целиком</Link>
               </div>
               <RIcon styleName='background'/>
             </div>
           </div>
           <div styleName='users'>
-            <button styleName='item' className={s.green}>
-              <div styleName='face'>
-                <img src='https://zengarden.in/wp-content/uploads/2012/10/tesla.jpg'/>
-              </div>
-              <div styleName='name'>
-                <span>SWIMING</span>
-                <p>Никола <br/>Тесла</p>
-              </div>
-            </button>
-            <button styleName='item' className={s.blue}>
-              <div styleName='face'>
-                <img src='http://books.atheism.ru/atheists/einstein2.jpg'/>
-              </div>
-              <div styleName='name'>
-                <span>CYCLING</span>
-                <p>Альберт <br/>Эйнштейн</p>
-              </div>
-            </button>
-            <button styleName='item' className={`${s.red} ${s.active}`}>
-              <div styleName='face'>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Muhammad_Ali_NYWTS.jpg/440px-Muhammad_Ali_NYWTS.jpg'/>
-              </div>
-              <div styleName='name'>
-                <span>RUNNING</span>
-                <p>Мохаммед <br/>Али</p>
-              </div>
-            </button>
-            <button styleName='item' className={s.yellow}>
-              <div styleName='face'>
-                <img src='https://24smi.org/public/media/235x307/celebrity/2017/11/22/nwv7kqyvs72t-alan-tiuring.jpg'/>
-              </div>
-              <div styleName='name'>
-                <span>RUNNING</span>
-                <p>Алан <br/>Тьюринг</p>
-              </div>
-            </button>
+            {reviews}
           </div>
         </div>
       </div>
--- src/reducers/App.js
+++ src/reducers/App.js
@@ -1,6 +1,54 @@
 import { } from '../constants/App'
 
-const initialState = { }
+const initialState = {
+  reviews: [
+    {
+      id: 1,
+      name: 'Никола Тесла',
+      type: 'scientist',
+      video: true,
+      face: 'https://zengarden.in/wp-content/uploads/2012/10/tesla.jpg',
+      img: 'https://ae01.alicdn.com/kf/HTB1KlsZNVXXXXa5apXXq6xXFXXX1/-.jpg_640x640.jpg',
+      color: 'red',
+      title: 'Пробеги полумарафон в Вероне',
+      text: 'Очень долго хотела скинуть лишний вес да и вообще начать вести здоровый образ жизни. Но все было некогда. Знакомые позвали на открытую тренировку в школу бега с мастером спорта. Чем черт не шутит, решила сходить, тем более интересно было узнать, почему.'
+    },
+    {
+      id: 2,
+      name: 'Альберт Эйнштейн',
+      type: 'scientist',
+      video: false,
+      face: 'https://euroislam.pl/app/uploads/2012/08/Albert-Einstein.jpg',
+      img: 'https://ae01.alicdn.com/kf/HTB1KlsZNVXXXXa5apXXq6xXFXXX1/-.jpg_640x640.jpg',
+      color: 'blue',
+      title: 'Пробеги полумарафон в Вероне',
+      text: 'Очень долго хотела скинуть лишний вес да и вообще начать вести здоровый образ жизни. Но все было некогда. Знакомые позвали на открытую тренировку в школу бега с мастером спорта. Чем черт не шутит, решила сходить, тем более интересно было узнать, почему.'
+    },
+    {
+      id: 3,
+      name: 'Мохаммед   Али',
+      type: 'boxer',
+      video: true,
+      face: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Muhammad_Ali_NYWTS.jpg/440px-Muhammad_Ali_NYWTS.jpg',
+      img: 'https://ae01.alicdn.com/kf/HTB1KlsZNVXXXXa5apXXq6xXFXXX1/-.jpg_640x640.jpg',
+      color: 'green',
+      title: 'Пробеги полумарафон в Вероне',
+      text: 'Очень долго хотела скинуть лишний вес да и вообще начать вести здоровый образ жизни. Но все было некогда. Знакомые позвали на открытую тренировку в школу бега с мастером спорта. Чем черт не шутит, решила сходить, тем более интересно было узнать, почему.'
+    },
+    {
+      id: 4,
+      name: 'Алан Тьюринг',
+      type: 'developer',
+      video: false,
+      face: 'https://24smi.org/public/media/235x307/celebrity/2017/11/22/nwv7kqyvs72t-alan-tiuring.jpg',
+      img: 'https://ae01.alicdn.com/kf/HTB1KlsZNVXXXXa5apXXq6xXFXXX1/-.jpg_640x640.jpg',
+      color: 'yellow',
+      title: 'Пробеги полумарафон в Вероне',
+      text: 'Очень долго хотела скинуть лишний вес да и вообще начать вести здоровый образ жизни. Но все было некогда. Знакомые позвали на открытую тренировку в школу бега с мастером спорта. Чем черт не шутит, решила сходить, тем более интересно было узнать, почему.'
+    }
+  ],
+  activeReview: 1
+}
 
 export default function appState (state = initialState, action) {
   switch (action.type) {
